2188bf1c30dc794b64ae6f5fef34bd8e7b15d76e
Karoline Klever <karoline.klever@epinova.no>
2014-01-20 15:21:07 +0100

    Added demo

---
+++ scripts/image/imageCtrl.js
@@ -0,0 +1,6 @@
+﻿imageModule.controller('ImageCtrl', function ($scope) {
+    $scope.dogs = [{ size: 'Large', breed: 'Golden Retriever' },
+        { size: 'Medium', breed: 'Kleiner Münsterländer' }];
+
+    $scope.dogsize = $scope.dogs[0];
+})
---
+++ scripts/movies/movieCtrl.js
@@ -0,0 +1,7 @@
+﻿function MovieCtrl($scope) {
+    $scope.movies = [
+        { title: 'The Shawshank Redemption', rating: '9.2', length: '142 min', director: 'Frank Darabont', stars: [{ name: 'Tim Robbins' }, { name: 'Morgan Freeman' }, { name: 'Bob Gunton' }], budget: 25000000 },
+        { title: 'The Godfather', rating: '9.2', length: '175 min', director: 'Francis Ford Coppola', stars: [{ name: 'Marlon Brando' }, { name: 'Al Pacino' }, { name: 'James Caan' }], budget: 6000000 },
+        { title: 'The Godfather: Part II', rating: '9.0', length: '200 min', director: 'Francis Ford Coppola', stars: [{ name: 'Al Pacino' }, { name: 'Robert De Niro' }, { name: 'Robert Duvall' }], budget: 13000000 },
+        { title: 'Pulp Fiction', rating: '8.9', length: '154 min', director: 'Quentin Tarantino', stars: [{ name: 'John Travolta' }, { name: 'Uma Thurman' }, { name: 'Samuel L. Jackson' }], budget: 8000000 }];
+}
